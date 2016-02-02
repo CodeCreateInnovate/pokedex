024a36d994d76e35ab096739aaaee8343f701633
ripnraks300 <ErickTruong@MacBook-Pro.home>
2016-02-01 23:56:27 -0500

    Finished Pokedex App.

---
+++ pokedex-by-ErickTruong/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-ErickTruong
+//
+//  Created by ERICK TRUONG on 1/31/16.
+//  Copyright © 2016 Code Create Innovate. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ pokedex-by-ErickTruong/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex-by-ErickTruong
+//
+//  Created by ERICK TRUONG on 1/22/16.
+//  Copyright © 2016 Code Create Innovate. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
